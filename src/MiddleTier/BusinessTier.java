c80102c4c74f7b2627389c271275ae09abf84261
Antonio Villarreal Jr <clown_krown@yahoo.com>
2016-04-28 12:11:26 -0500

    Function Stubs for Business Tier

--- src/MiddleTier/BusinessTier.java
+++ src/MiddleTier/BusinessTier.java
@@ -1,7 +1,24 @@
 package MiddleTier;
 
+
+import edu.stanford.nlp.trees.Tree;
+
 /**
  * Created by Tony on 4/28/2016.
  */
 public class BusinessTier {
+    public BusinessTier(){
+
+    }
+
+    public boolean determineQuestion(String qeustion, Tree tree){
+        String query = "";
+
+        return evaluateAnswer(query);
+    }
+
+    public boolean evaluateAnswer(String query){
+
+        return true;
+    }
 }

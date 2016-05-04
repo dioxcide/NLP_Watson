e5cbd6735d7536afb29b77b55f8a46884774f3b9
killerkev <kevthebboy@gmail.com>
2016-05-03 19:04:30 -0500

    Removed redundency in WordProperty.

--- src/Label/Labeler.java
+++ src/Label/Labeler.java
@@ -43,49 +43,6 @@ public class Labeler {
         return result;
     }
 
-    private static WordProperty processNER(String w){
-
-        ArrayList<WordProperty> tupleList = new ArrayList<WordProperty>();
-
-        // creates a StanfordCoreNLP object, with POS tagging, lemmatization, NER, parsing, and coreference resolution
-        Properties props = new Properties();
-        props.put("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
-        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-        WordProperty temp = null;
-
-        // read some text from the file..
-        //File inputFile = new File("src/test/resources/sample-content.txt");
-
-        // create an empty Annotation just with the given text
-        Annotation document = new Annotation(w);
-
-        // run all Annotators on this text
-        pipeline.annotate(document);
-
-        // these are all the sentences in this document
-        // a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
-        List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
-
-        for(CoreMap sentence: sentences) {
-            // traversing the words in the current sentence
-            // a CoreLabel is a CoreMap with additional token-specific methods
-            for (CoreLabel token: sentence.get(CoreAnnotations.TokensAnnotation.class)) {
-
-                // this is the text of the token
-                String word = token.get(CoreAnnotations.TextAnnotation.class);
-
-                // this is the NER label of the token
-                String ne = token.get(CoreAnnotations.NamedEntityTagAnnotation.class);
-
-                //System.out.println("word: " + word + " ne:" + ne);
-
-                temp = new WordProperty(word,ne);
-                return temp;
-            }
-
-        }
-        return temp;
-    }
 
     private static String associateTable(String word)
     {
@@ -100,49 +57,6 @@ public class Labeler {
         else if(ArrayUtils.contains(movieList,word)) return     "Movie";
 
         return "N/A";
-    }
-
-    private static String processNERWord(String w){
-
-
-        // creates a StanfordCoreNLP object, with POS tagging, lemmatization, NER, parsing, and coreference resolution
-        Properties props = new Properties();
-        props.put("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
-        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-        String temp = null;
-
-        // read some text from the file..
-        //File inputFile = new File("src/test/resources/sample-content.txt");
-
-        // create an empty Annotation just with the given text
-        Annotation document = new Annotation(w);
-
-        // run all Annotators on this text
-        pipeline.annotate(document);
-
-        // these are all the sentences in this document
-        // a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
-        List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
-
-        for(CoreMap sentence: sentences) {
-            // traversing the words in the current sentence
-            // a CoreLabel is a CoreMap with additional token-specific methods
-            for (CoreLabel token: sentence.get(CoreAnnotations.TokensAnnotation.class)) {
-
-                // this is the text of the token
-                String word = token.get(CoreAnnotations.TextAnnotation.class);
-
-                // this is the NER label of the token
-                String ne = token.get(CoreAnnotations.NamedEntityTagAnnotation.class);
-
-                //System.out.println("word: " + word + " ne:" + ne);
-
-                temp = ne;
-                return temp;
-            }
-
-        }
-        return temp;
     }
 
     private static void mapWords(ArrayList<WordProperty> importantWords)
--- src/Label/wordProperty.java
+++ src/Label/wordProperty.java
@@ -7,13 +7,11 @@ public class WordProperty {
 
     public String word;
     public String POStag;
-    public String NERtag;
     public String table;
     public WordProperty(String word, String POStag)
     {
         this.word = word;
         this.POStag = POStag;
         this.table = "N/A";
-        this.NERtag = "";
     }
 }
